aa1add338a172cf2d90402ab52345099b9d27822
vinicabral9 <vini.cabral.basilio@gmail.com>
2024-09-18 09:27:04 -0300

    aula dia 18/09

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { NativeBaseProvider } from 'native-base';
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import AppNavigator from './src/navigation/AppNavigator';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <NavigationContainer>
+        <AppNavigator />
+      </NavigationContainer>
+    </NativeBaseProvider>
   );
 }
 
